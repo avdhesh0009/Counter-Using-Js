089e72c3f95847e455e7da44a6f8e9638be06ea8
Avdhesh Kumar <avdheshkumar41237@gmail.com>
2023-10-13 16:37:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Mera Logic
+// document.getElementById('plus').addEventListener('click',function(){
+//    document.getElementById('content').innerHTML++;
+// });
+// document.getElementById('minus').addEventListener('click',function(){
+//     document.getElementById('content').innerHTML--;
+// })
+
+const countValue=document.querySelector('#counter');
+const increment = ()=>{
+   let value=parseInt(countValue.innerText);
+   value=value+1;
+   countValue.innerText=value;
+}
+const decrement = ()=>{
+    let value=parseInt(countValue.innerText);
+    value=value-1;
+    countValue.innerText=value;
+}
